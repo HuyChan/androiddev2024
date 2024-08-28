c6846d7f0d3564177d23138a5821349baee18a18
Tran Huy <tranngochuy2611@gmail.com>
2024-08-28 14:27:47 +0700

    parctical 3

--- app/src/main/java/vn/edu/usth/weather/MainActivity.java
+++ app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -8,7 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
